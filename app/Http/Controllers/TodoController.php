6c4ab64789a09c4da75e5bcc6f927a621fc73eb9
Rennu Aq'sha Sanjaya <rennuaqsha12@gmail.com>
2024-05-04 13:26:00 +0800

    Update, Deleted Data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -19,7 +19,10 @@ class TodoController extends Controller
 
         // dd($todos);
 
-        return view('todo.index', compact('todos'));
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->count();
+        return view('todo.index', compact('todos', 'todosCompleted'));
     }
 
     /**
@@ -60,7 +63,14 @@ class TodoController extends Controller
      */
     public function edit(Todo $todo)
     {
-        return view('todo.edit');
+        if (auth()->user()->id == $todo->user_id) {
+            // dd($todo);
+            return view('todo.edit', compact('todo'));
+        } else {
+            // abort(403);
+            // abort(403, 'Not authorized');
+            return redirect()->route('todo.index')->with('danger','You are not authorized to edit this todo!');
+        }
     }
 
     /**
@@ -68,7 +78,44 @@ class TodoController extends Controller
      */
     public function update(Request $request, Todo $todo)
     {
-        //update sesuatu
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        // Practical
+        // $todo->title = $request->title;
+        // $todo->save();
+
+        // Eloquent Way - Readable
+        $todo->update([
+            'title' => ucfirst($request->title),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo updated successfully!');
+    }
+
+    public function complete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->update([
+                'is_complete' => true,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo completed successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to complete this todo!');
+        }
+    }
+
+    public function uncomplete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+
+            $todo->update([
+                'is_complete' => false,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo uncompleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to uncomplete this todo!');
+        }
     }
 
     /**
@@ -76,6 +123,25 @@ class TodoController extends Controller
      */
     public function destroy(Todo $todo)
     {
-        //hapus data
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->delete();
+            return redirect()->route('todo.index')->with('success', 'Todo deleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to delete this todo!');
+        }
+    }
+
+    public function destroyCompleted()
+    {
+        // get all todos for current user where is_completed is true
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->get();
+        foreach ($todosCompleted as $todo) {
+            $todo->delete();
+        }
+        // dd($todosCompleted);
+        return redirect()->route('todo.index')->with('success', 'All completed todos deleted successfully!');
     }
 }
+
---
+++ tugas6.md
@@ -0,0 +1,33 @@
+# Tugas 6
+
+## 1. Todo Complete
+
+![Todo Compelete](screenshoot/tugas6/1.png)
+
+# 2. Todo Uncomplete
+
+![Todo Uncomplete](screenshoot/tugas6/2.png)
+
+## 3. todo Update
+
+![Todo Update](screenshoot/tugas6/3.png)
+
+## 4. Todo Delete
+
+![Todo Delete](screenshoot/tugas6/4.png)
+
+## 5. Todo All Complete Delete
+
+![Todo All Compelete Delete](screenshoot/tugas6/5.png)
+
+# 6. User Uncomplete
+
+![User Make Adminn](screenshoot/tugas6/6.png)
+
+## 7. User Remove
+
+![User Remove](screenshoot/tugas6/7.png)
+
+## 8. User Deleted
+
+![User Deleted](screenshoot/tugas6/8.png)
